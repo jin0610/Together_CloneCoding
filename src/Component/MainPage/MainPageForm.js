ed0a6064a43f45a19201030c1d74952e8027a1c3
jin0610 <sji302777@gmail.com>
2022-08-07 01:55:48 +0900

    리액트 초기 설정

---
+++ src/Component/MainPage/MainPageForm.js
@@ -0,0 +1,13 @@
+//<li class="slide_pannel" data-index="1" style="width: 100%; transform: translateX(-100%); transition-duration: 500ms; white-space: normal; position: absolute;"><a aria-hidden="true" navigateinternalhref="" data-tiara-action-name="홈커버_클릭" class="link_slide" href="https://together.kakao.com/fundraisings/99024"><div class="area_cont"><span class="tag_slide">EBS나눔0700</span><span class="tit_slide">상규야,
+//아빠가 지켜줄게</span><span class="desc_slide ellipsis_type1">"처음 수술했을 때 6개월 시한부 선고 받았어요"
+//상규는 뇌종양 교모세포종 희소 암으로 생사의 갈림길에 서 있습니다.
+//상규가 항암 치료를 잘 받아 학교로 돌아갈 수 있도록 관심이 필요해요.</span></div><span class="area_thumb"><span class="img_thumb" style="background-image: url(&quot;https://mud-kage.kakaocdn.net/dn/XGrGu/btrIJ7JzZ4S/7z6z9nPPiLUkVggeHNTwp1/c360.jpg&quot;);"></span></span></a></li>
+const MainPageForm = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default MainPageForm;
---
+++ src/Pages/CreateAccountPage.js
@@ -0,0 +1,10 @@
+
+const CreateAccountPage = () =>{
+    return (
+        <div>
+            회원가입일걸?
+        </div>
+    )
+}
+
+export default CreateAccountPage;
